6254954313c5176c4dffeaa83ab98c01be7d7225
Marat Baikin <3371335@gmail.com>
2023-10-19 17:29:25 +0300

    create

---
+++ src/App.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { App } from './components/App';
+
+import { User } from './components/App/App.types';
+
+const user: User = {
+  id: 1,
+  name: 'Marat',
+  email: 'test@example.com',
+}
+
+test('renders learn react link', () => {
+  render(<App user={ user} />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
